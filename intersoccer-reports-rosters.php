028c59e944355dac077653b08fe5521934521ed6
Jeremy Lee <jlee@samaritan.org>
2025-11-24 13:07:38 -0500

    Bump version to 1.11.24

--- intersoccer-reports-rosters.php
+++ intersoccer-reports-rosters.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: InterSoccer Reports and Rosters
  * Description: Generates event rosters and reports for InterSoccer Switzerland admins using WooCommerce data.
- * Version: 1.11.17
+ * Version: 1.11.24
  * Author: Jeremy Lee
  * Text Domain: intersoccer-reports-rosters
  * License: GPL-2.0+
